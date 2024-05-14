2031b25824efccd4c26320e1cb70c415b1158217
pon02 <rkis_taro@icloud.com>
2024-05-14 18:42:22 +0900

    mapperパッケージにOrderMapperインターフェースを作成

---
+++ src/main/java/com/pon02/Assignment10/mapper/OrderMapper.java
@@ -0,0 +1,12 @@
+package com.pon02.Assignment10.mapper;
+
+import com.pon02.Assignment10.entity.Order;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+@Mapper
+public interface OrderMapper {
+    @Select("SELECT * FROM orders")
+    List<Order> OrdersFindAll();
+}

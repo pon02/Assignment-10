830d1400e8c8706febfef166a17cbd7f70a1afd7
pon02 <rkis_taro@icloud.com>
2024-05-15 15:04:03 +0900

    URIの記述をケバブケースに変更

--- src/main/java/com/pon02/Assignment10/controller/CarTypeController.java
+++ src/main/java/com/pon02/Assignment10/controller/CarTypeController.java
@@ -16,12 +16,12 @@ public class CarTypeController {
         this.carTypeService = carTypeService;
     }
 
-    @GetMapping("/cartypes")
+    @GetMapping("/car-types")
     public List<CarType> getCarTypes() {
         return carTypeService.findAllCarTypes();
     }
 
-    @GetMapping("/cartypes/{id}")
+    @GetMapping("/car-types/{id}")
     public CarType getCarTypeById(@PathVariable int id) {
         return carTypeService.findCarTypeById(id);
     }

dcd5965642e744bca3d3e8c4238f1bcb63abfa9e
pon02 <rkis_taro@icloud.com>
2024-05-21 18:46:55 +0900

    ordersとstaffsテーブルのCREATED_ATとUPDATED_ATをTimeStamp型からDateTime型に変更

--- sql/orders.sql
+++ sql/orders.sql
@@ -4,6 +4,7 @@ CREATE TABLE orders (
   id int unsigned AUTO_INCREMENT,
   car_type_id int unsigned NOT NULL,
   order_status_id int unsigned NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime NOT NULL DEFAULT 0000-00-00 00:00:00 ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(id)
 );

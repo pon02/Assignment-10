3d5aa701e9d26a9100f964e30cb577cc1e7048d5
pon02 <rkis_taro@icloud.com>
2025-01-27 22:39:31 +0900

    EntityにSectionクラスを追加

---
+++ src/main/java/com/pon02/Assignment10/validation/uniqueName/UniqueNameValidator.java
@@ -0,0 +1,47 @@
+package com.pon02.Assignment10.validation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UniqueNameValidator implements ConstraintValidator<UniqueName, String> {
+
+    private final UniqueNameChecker carTypeNameChecker;
+    private final UniqueNameChecker sectionNameChecker;
+    private Class<?extends UniqueNameChecker>checkerClass;
+
+
+    @Autowired
+    public UniqueNameValidator(
+        @Qualifier("carTypeNameChecker") UniqueNameChecker carTypeNameChecker,
+        @Qualifier("sectionNameChecker") UniqueNameChecker sectionNameChecker) {
+        this.carTypeNameChecker = carTypeNameChecker;
+        this.sectionNameChecker = sectionNameChecker;
+    }
+
+    @Override
+    public void initialize(UniqueName constraintAnnotation) {
+        this.checkerClass = constraintAnnotation.checker();
+    }
+
+    @Override
+    public boolean isValid(String name, ConstraintValidatorContext context) {
+        if (name == null) {
+            return true;
+        }
+        UniqueNameChecker checker = getCheckerInstance();
+        return checker.existsByName(name);
+    }
+
+    private UniqueNameChecker getCheckerInstance() {
+        if (checkerClass == CarTypeNameChecker.class) {
+            return carTypeNameChecker;
+        } else if (checkerClass == SectionNameChecker.class) {
+            return sectionNameChecker;
+        }
+        throw new IllegalArgumentException("Unsupported checker class");
+    }
+}

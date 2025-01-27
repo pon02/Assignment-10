85815a7728cda0eea6b9a94337f623864e3f86a6
pon02 <rkis_taro@icloud.com>
2025-01-27 18:37:11 +0900

    StaffNotFoundExceptionの追加

---
+++ src/main/java/com/pon02/Assignment10/exception/StaffNotFoundException.java
@@ -0,0 +1,8 @@
+package com.pon02.Assignment10.exception;
+
+public class StaffNotFoundException extends RuntimeException{
+    public StaffNotFoundException(String message) {
+        super(message);
+    }
+
+}

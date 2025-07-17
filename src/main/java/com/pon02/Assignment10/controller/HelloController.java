f8b03eab4d8bc7298c103c920a5bd75f633de601
pon02 <rkis_taro@icloud.com>
2025-07-17 21:44:34 +0900

    Deploy確認用HelloControllerの追加

---
+++ src/main/java/com/pon02/Assignment10/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.pon02.Assignment10.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello, Assignment10 app is deployed!";
+  }
+}

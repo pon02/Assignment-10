5d75c434bd0d8f849ceaa55cee916748e18dddc7
pon02 <rkis_taro@icloud.com>
2025-07-24 17:32:53 +0900

    data.sql,schema.sql追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO car_types (car_type, capacity) VALUES ("セダン4人", 4);
+INSERT INTO car_types (car_type, capacity) VALUES ("ハコバン7人", 7);
+INSERT INTO car_types (car_type, capacity) VALUES ("ハイエース9人", 9);
+INSERT INTO car_types (car_type, capacity) VALUES ("小型バス", 21);
+
+INSERT INTO sections (section_name) VALUES ("大道具");
+INSERT INTO sections (section_name) VALUES ("音響");
+INSERT INTO sections (section_name) VALUES ("照明");
+INSERT INTO sections (section_name) VALUES ("特効");
+
+INSERT INTO order_statuses (order_status) VALUES ("配車中");
+INSERT INTO order_statuses (order_status) VALUES ("到着");
+INSERT INTO order_statuses (order_status) VALUES ("出発済");
+INSERT INTO order_statuses (order_status) VALUES ("取消");
+
+INSERT INTO staff_statuses (staff_status) VALUES ("待機中");
+INSERT INTO staff_statuses (staff_status) VALUES ("出発済");
+INSERT INTO staff_statuses (staff_status) VALUES ("取消");
